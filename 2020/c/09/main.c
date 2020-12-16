a55386838780f481e6c709472dff509caad2e4e9
Tyler Gould <tg@tylergould.net>
2020-12-16 16:55:19 -0600

    read the numbers

---
+++ 2020/c/09/main.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define OP_LENGTH 3
+
+char* load_input(FILE *fp) {
+	fseek(fp, 0, SEEK_END);
+	size_t length = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	char *buffer = malloc(length + 1);
+	fread(buffer, sizeof(char), length, fp);
+	return buffer;
+}
+
+typedef struct reader {
+	char* text;
+	char current_ch;
+	int position;
+} reader;
+
+reader new_reader(char* text) {
+	reader r;
+	r.text = text;
+	r.position = 0;
+	r.current_ch = text[0];
+	return r;
+}
+
+int reader_input_ended(reader* r) {
+	return r->current_ch == '\0';
+}
+
+typedef struct intlist {
+	int* numbers;
+	int size;
+} intlist;
+
+intlist new_intlist() {
+	intlist list;
+	list.size = 0;
+	return list;
+}
+
+void intlist_add(intlist* ilist, int val) {
+	int new_size = ilist->size + 1;
+	if (ilist->size == 0) {
+		ilist->numbers = malloc(new_size * sizeof(int));
+	} else {
+		ilist->numbers = realloc(ilist->numbers, new_size * sizeof(int));
+	}
+	(ilist->numbers)[ilist->size] = val;
+	ilist->size++;
+}
+
+void move_reader_to_offset(reader* r, int offset) {
+	r->position = offset;
+	r->current_ch = (r->text)[offset];
+}
+
+void read_next(reader* r) {
+	move_reader_to_offset(r, r->position + 1);
+}
+
+void move_reader_to_next_line(reader* r) {
+	if (r->current_ch == '\n') {
+		read_next(r);
+		return;
+	}
+	while (r->current_ch != '\0') {
+		read_next(r);
+		if (r->current_ch == '\n') {
+			read_next(r);
+			break;
+		}
+	}
+}
+
+int read_integer(reader* r) {
+	int count = strtoul(r->text + r->position, NULL, 10);
+	int num_digits = 0;
+	int result = count;
+	if (count == 0) {
+		num_digits = 1;
+	} else {
+		while (result != 0) {
+			result /= 10;
+			num_digits++;
+		}
+	}
+	move_reader_to_offset(r, r->position + num_digits);
+	return count;
+}
+
+intlist read_integer_list(reader* r) {
+	intlist il = new_intlist();
+	while (reader_input_ended(r) == 0) {
+		int num = read_integer(r);
+		intlist_add(&il, num);
+		move_reader_to_next_line(r);
+	}
+	return il;
+}
+
+
+void part_one(intlist* ilist) {
+	for (int i = 0; i < ilist->size; i++) {
+		printf("%d\n", *(ilist->numbers + i));
+	}
+}
+
+void part_two() {
+	printf("TODO\n");
+}
+
+
+int main() {
+	FILE *fp;
+	fp = fopen("input_test", "r");
+	if (fp == NULL) {
+		puts("could not open file");
+		return 1;
+	}
+	char *input = load_input(fp);
+	if (input == NULL) {
+		puts("could not malloc");
+		return 1;
+	}
+	fclose(fp);
+
+	reader r = new_reader(input);
+	intlist il = read_integer_list(&r);
+	printf("part1:\n");
+	part_one(&il);
+
+	printf("\npart2:\n");
+	part_two();
+	printf("\n");
+
+	return 0;
+}
