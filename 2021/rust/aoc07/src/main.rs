7a26c42ac3cf719a8aba38047dbb90638b7016b0
Tyler Gould <tg@tylergould.net>
2021-12-24 22:21:31 -0600

    start day 1

---
+++ 2021/rust/aoc07/src/main.rs
@@ -0,0 +1,32 @@
+
+pub fn part_one(input: &String) -> usize {
+    println!("{}", input);
+    5
+}
+
+fn main() {
+    let contents = std::fs::read_to_string("input").unwrap();
+    let result = part_one(&contents);
+    println!("part 1");
+    println!("{:?}", result);
+    // let result = part_two(&contents);
+    // println!("part 2");
+    // println!("{:?}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    fn it_works() {
+        let example = String::from("16,1,2,0,4,2,7,1,2,14");
+        assert_eq!(part_one(&example), 37);
+    }
+    // #[test]
+    // fn it_works_v2() {
+    //     let example = String::from("16,1,2,0,4,2,7,1,2,14");
+    //     assert_eq!(part_two(&example), 26984457539);
+    // }
+}
