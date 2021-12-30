916a1471e4651a9ea930832dd37add7dd201fe9f
Tyler Gould <tg@tylergould.net>
2021-12-30 14:41:14 -0600

    day 10 part 1 rust 2021

--- 2021/rust/aoc10/src/main.rs
+++ 2021/rust/aoc10/src/main.rs
@@ -1,6 +1,50 @@
-pub fn part_one(input: &String) -> isize {
-    println!("{}", input);
-    0
+type Line = Vec<char>;
+type Input = Vec<Line>;
+
+fn parse_input(input: &String) -> Input {
+    input.lines().map(|line| line.chars().collect()).collect()
+}
+
+fn get_matching_opening_bracket_for_closer(ch: char) -> char {
+    match ch {
+        ']' => '[',
+        ')' => '(',
+        '}' => '{',
+        '>' => '<',
+        _ => unreachable!("whut")
+    }
+}
+
+pub fn part_one(input: &String) -> u32 {
+    let openers = String::from("[({<");
+    let closers = String::from("])}>");
+    // let openers_to_closers = HashMap::
+    let parsed = parse_input(input);
+    let mut stack: Vec<char> = vec![];
+    let mut illegal_score: Vec<u32> = vec![];
+    parsed.iter().for_each(|line| {
+        for &ch in line.iter() {
+            if openers.find(|c| c == ch).is_some() {
+                stack.push(ch);
+            } else if closers.find(|c| c == ch).is_some() {
+                let opener = get_matching_opening_bracket_for_closer(ch);
+                let last_ch = stack.pop().unwrap();
+                if last_ch != opener {
+                    illegal_score.push(match ch {
+                        ')' => 3,
+                        ']' => 57,
+                        '}' => 1197,
+                        '>' => 25137,
+                        _ => unreachable!()
+                    });
+                    break;
+                }
+            } else {
+                unreachable!("char not one of accepted brackets");
+            }
+        };
+    });
+    illegal_score.iter().sum()
   }
   
   fn main() {
