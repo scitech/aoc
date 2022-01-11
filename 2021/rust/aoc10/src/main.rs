4810cc24eb694544936ca24a893f6ca4a090f49f
Tyler Gould <tg@tylergould.net>
2022-01-10 21:53:56 -0600

    day 10 part 2 lazy

--- 2021/rust/aoc10/src/main.rs
+++ 2021/rust/aoc10/src/main.rs
@@ -18,7 +18,6 @@ fn get_matching_opening_bracket_for_closer(ch: char) -> char {
 pub fn part_one(input: &String) -> u32 {
     let openers = String::from("[({<");
     let closers = String::from("])}>");
-    // let openers_to_closers = HashMap::
     let parsed = parse_input(input);
     let mut stack: Vec<char> = vec![];
     let mut illegal_score: Vec<u32> = vec![];
@@ -45,20 +44,68 @@ pub fn part_one(input: &String) -> u32 {
         };
     });
     illegal_score.iter().sum()
-  }
+}
+pub fn part_two(input: &String) -> i128 {
+    let openers = String::from("[({<");
+    let closers = String::from("])}>");
+    let parsed = parse_input(input);
+    let mut stack: Vec<char> = vec![];
+    let mut incomplete_lines: Vec<Line> = vec![];
+    parsed.iter().for_each(|line| {
+        let mut chars_read = 0;
+        for &ch in line.iter() {
+            chars_read += 1;
+            if openers.find(|c| c == ch).is_some() {
+                stack.push(ch);
+            } else if closers.find(|c| c == ch).is_some() {
+                let opener = get_matching_opening_bracket_for_closer(ch);
+                let last_ch = stack.pop().unwrap();
+                if last_ch != opener {
+                    stack.clear();
+                    break;
+                }
+            } else {
+                unreachable!("char not one of accepted brackets");
+            }
+            if chars_read == line.len() {
+                incomplete_lines.push(stack.clone());
+                stack.clear();
+            }
+        };
+    });
+    let mut result: Vec<i128> = vec![];
+    incomplete_lines.iter().for_each(|line| {
+        let mut reversable = line.clone();
+        reversable.reverse();
+        let mut score: i128 = 0;
+        for &ch in reversable.iter() {
+            score = (5 * score) + (match ch {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => unreachable!()
+            });
+        }
+        result.push(score);
+    });
+    result.sort();
+    let mid = result.len() / 2;
+    result[mid]
+}
   
-  fn main() {
+fn main() {
     let contents = std::fs::read_to_string("input").unwrap();
     let result = part_one(&contents);
     println!("part 1");
     println!("{:?}", result);
-    // let result = part_two(&contents);
-    // println!("part 2");
-    // println!("{:?}", result);
-  }
+    let result = part_two(&contents);
+    println!("part 2");
+    println!("{:?}", result);
+}
   
-  #[cfg(test)]
-  mod tests {
+#[cfg(test)]
+mod tests {
     use super::*;
   
   
@@ -76,9 +123,18 @@ pub fn part_one(input: &String) -> u32 {
 <{([{{}}[<[[[<>{}]]]>[]]");
         assert_eq!(part_one(&example), 26397);
     }
-    // #[test]
-    // fn it_works_v2() {
-    //     let example = String::from("16,1,2,0,4,2,7,1,2,14");
-    //     assert_eq!(part_two(&example), 168);
-    // }
-  }
+    #[test]
+    fn it_works_v2() {
+        let example = String::from("[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]");
+        assert_eq!(part_two(&example), 288957);
+    }
+}
