d04f0eb695e8084c2a58dfecb5ec4e805242ddd7
Tyler Gould <tg@tylergould.net>
2022-12-03 09:37:13 -0600

    learned about chunks

--- 2022/rust/day03/src/main.rs
+++ 2022/rust/day03/src/main.rs
@@ -1,12 +1,15 @@
-struct Rucksack {
-	raw: String,
-	compartments: [String; 2],
-}
-
 fn same_char_finder(a: &String, b: &String) -> Result<char, String> {
 	a.chars()
 		.find(|a_ch| b.chars().find(|b_ch| a_ch == b_ch).is_some())
 		.ok_or(String::from("could not find same char in both strings"))
+}
+
+fn same_char_finder_2(a: &String, b: &String, c: &String) -> Result<char, String> {
+	a.chars()
+		.find(|a_ch| {
+			b.chars().find(|b_ch| a_ch == b_ch).is_some() && c.chars().find(|c_ch| a_ch == c_ch).is_some()
+		})
+		.ok_or(String::from("could not find same char in all strings"))
 }
 
 fn get_priority(ch: char) -> usize {
@@ -17,22 +20,30 @@ fn get_priority(ch: char) -> usize {
 	}
 }
 
-fn score_priorities(sacks: &Vec<Rucksack>) -> usize {
-	sacks.iter().fold(0, |total, sack| {
-		let ch = same_char_finder(&sack.compartments[0], &sack.compartments[1]).unwrap();
-		total + get_priority(ch)
-	})
+pub fn part_one(input: &String) -> usize {
+	input.lines()
+		.map(|raw_line| get_priority(
+			same_char_finder(
+				&String::from(&raw_line[0..(raw_line.len() / 2)]),
+				&String::from(&raw_line[(raw_line.len() / 2)..raw_line.len()]),
+			).unwrap()
+		))
+		.sum()
 }
 
-pub fn part_one(input: &String) -> usize {
-	let rucksacks: Vec<Rucksack> = input.lines().map(|raw_line| Rucksack{ 
-		raw: String::from(raw_line), 
-		compartments: [
-			String::from(&raw_line[0..(raw_line.len() / 2)]),
-			String::from(&raw_line[(raw_line.len() / 2)..raw_line.len()]),
-		]
-	}).collect();
-	score_priorities(&rucksacks)
+pub fn part_two(input: &String) -> usize {
+	input.lines()
+		// iterator is line-by-line, need to collect to make chunks
+		.collect::<Vec<&str>>() 
+		.chunks(3)
+		.map(|chunk| get_priority(
+			same_char_finder_2(
+				&String::from(chunk[0]), 
+				&String::from(chunk[1]), 
+				&String::from(chunk[2])).unwrap()
+			)
+		)
+		.sum()
 }
 
 fn main() {
@@ -40,9 +51,9 @@ fn main() {
 	let result = part_one(&contents);
 	println!("part 1");
 	println!("{:?}", result);
-	// let result = part_two(&contents);
-	// println!("part 2");
-	// println!("{:?}", result);
+	let result = part_two(&contents);
+	println!("part 2");
+	println!("{:?}", result);
 }
 	
 #[cfg(test)]
@@ -60,9 +71,14 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw");
 		assert_eq!(part_one(&example), 157);
 	}
-	// #[test]
-	// fn it_works_v2() {
-	//     let example = String::from("16,1,2,0,4,2,7,1,2,14");
-	//     assert_eq!(part_two(&example), 168);
-	// }
+	#[test]
+	fn it_works_v2() {
+		let example = String::from("vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw");
+		assert_eq!(part_two(&example), 70);
+	}
 }
