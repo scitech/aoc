efc90d46819b1e1b5f9959b7eb56ba93ac531e3e
Tyler Gould <tg@tylergould.net>
2022-12-07 00:50:07 -0600

    factor out dir reader

--- 2022/rust/day07/src/main.rs
+++ 2022/rust/day07/src/main.rs
@@ -1,4 +1,4 @@
-pub fn part_one(input: &String) -> usize {
+fn read_dir_stats(input: &String) -> Vec<(String, usize)> {
     let mut cwd: Vec<String> = vec![String::from("/")];
     let mut dirs: Vec<(String, usize)> = vec![(String::from("/"), 0)];
     for line in input.lines() {
@@ -41,6 +41,11 @@ pub fn part_one(input: &String) -> usize {
             }).collect();
         }
     }
+    dirs
+}
+
+pub fn part_one(input: &String) -> usize {
+    let dirs = read_dir_stats(input);
     let result = dirs.iter().fold(0, |total, dir_info| {
         let amount = dir_info.1;
         if amount <= 100000 {
@@ -51,48 +56,7 @@ pub fn part_one(input: &String) -> usize {
     result
 }
 pub fn part_two(input: &String) -> usize {
-    let mut cwd: Vec<String> = vec![String::from("/")];
-    let mut dirs: Vec<(String, usize)> = vec![(String::from("/"), 0)];
-    for line in input.lines() {
-        if line.chars().nth(0).unwrap() == '$' {
-            let parts: Vec<&str> = line.split(" ").collect();
-            if parts[1] == "cd" {
-                if parts[2] == ".." {
-                    cwd.pop();
-                } else {
-                    cwd.push(String::from(parts[2]));
-                }
-                continue;
-            } else if parts[1] == "ls" {
-                continue;
-            }
-        } else if &line[..3] == "dir" {
-            let parts: Vec<&str> = line.split(" ").collect();
-            let dir_name = String::from(parts[1]);
-            let resolved = if cwd.len() == 1 {
-                format!("/{dir_name}")
-            } else {
-                let mut copy = cwd[1..].to_vec();
-                copy.push(dir_name);
-                let joined = copy.join("/");
-                format!("{joined}")
-            };
-            let dir_info = dirs.iter().find(|(name, _)| name == &resolved);
-            if dir_info.is_none() {
-                dirs.push((resolved, 0));
-            }
-        } else /* a file */ {
-            let parts: Vec<&str> = line.split(" ").collect();
-            let size = parts[0].parse::<usize>().unwrap();
-            dirs = dirs.iter().map(|dir_stats| {
-
-                if cwd.join("/").contains(&dir_stats.0) {
-                    return (dir_stats.0.clone(), dir_stats.1 + size);
-                }
-                (dir_stats.0.clone(), dir_stats.1)
-            }).collect();
-        }
-    }
+    let dirs = read_dir_stats(input);
     let target = 30000000;
     let free = 70000000 - dirs[0].1;
     let min = target - free;
